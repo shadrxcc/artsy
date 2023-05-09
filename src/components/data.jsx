215b8c348f93f14ffa199f666e889140cc418fea
shadrxcc <akaadesm@gmail.com>
2023-05-09 02:08:56 +0100

    implement responsive header

---
+++ src/components/data.jsx
@@ -0,0 +1,35 @@
+import search from '../assets/searchicon.svg'
+import cart from '../assets/carticon.svg'
+// import notification from '../assets/notificationicon.svg'
+
+export const navlink = [
+    {
+        link: 'Home'
+    },
+    {
+        link: 'Marketplace'
+    },
+    {
+        link: 'Auction'
+    },
+    {
+        link: 'Drop'
+    },
+]
+
+export const icons = [
+    {
+        icon: search
+    },
+    {
+        icon: cart
+    },
+    // {
+    //     icon: notification
+    // }
+]
+
+//auction https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/auction.json
+// carusel https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/carousel.json
+// drop https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/drops.json
+// products https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/products.json
---
+++ src/components/header.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import artsylogo from "../assets/ARTSY..svg";
+import { icons, navlink } from "./data";
+import hamburger from "../assets/hamburger.svg";
+import close from "../assets/x.svg";
+import chat from "../assets/chat.svg";
+
+const Header = () => {
+  const [openmenu, setOpenMenu] = useState(false);
+
+  const openToggle = () => {
+    setOpenMenu(!openmenu);
+  };
+
+  const closeToggle = () => {
+    setOpenMenu(!openmenu);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center px-3 md:px-0 py-[22px] md:py-[45px] bg-white fixed w-full justify-around">
+        <div className="md:hidden">
+          <img onClick={openToggle} src={hamburger} alt="" />
+        </div>
+        <div className="m-auto md:m-0">
+          <img className="w-[5em] md:w-[7em]" src={artsylogo} alt="logo" />
+        </div>
+
+        <div>
+          <ul className="md:flex hidden gap-[47px]">
+            {navlink.map((link) => {
+              return (
+                <>
+                  <li>{link.link}</li>
+                </>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="flex gap-x-[11px]">
+          {icons.map((icon) => {
+            return <img className="w-[2em]" src={icon.icon} alt="" />;
+          })}
+        </div>
+      </div>
+
+      <div
+        id="menu"
+        className={`${
+          openmenu ? "#menu active" : "#menu"
+        } md:hidden active-menu bg-white py-[24px] px-[30px] left-[-30em] flex flex-col gap-y-[50px] absolute h-5/6 w-full`}
+      >
+        <div className="flex justify-between">
+          <img className="w-[5em]" src={artsylogo} alt="logo" />
+          <img onClick={closeToggle} src={close} alt="" />
+        </div>
+
+        <div>
+          <ul className="flex flex-col gap-[18px]">
+            {navlink.map((link) => {
+              return (
+                <>
+                  <li className="leading-52px text-[24px] font-[500]">
+                    {link.link}
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="h-3/6 flex justify-end items-end">
+          <img src={chat} className="float-right" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
